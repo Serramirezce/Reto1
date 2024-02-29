09697198c10028a1ebd8384de538eb23cd8c866a
Sergio Andrés Ramírez Céspedes <145515865+Serramirezce@users.noreply.github.com>
2024-02-28 21:50:52 -0500

    Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,15 +2,15 @@
 
 word = input("Ingrese una palabra: ")
 
-word = word.lower()
+word = word.lower() # Este método se usa para llevar todo el input a minúsculas y que al verificar que sea palíndromo, no tenga problemas con mayúsculas y minúsculas
 
-def invert_word(word):
+def invert_word(word): #Esfta función invierte la palabra
     inverted = ""
     for i in range(len(word)-1, -1, -1):
         inverted += word[i]
     return inverted
 
-def verdict(inverted):
+def verdict(inverted):  # Esta función evalúa si la palabra que retorna la función anterior es igual a la ingresada por el usuario
   if word == inverted:
     return "La palabra es un palíndromo"
   else :
