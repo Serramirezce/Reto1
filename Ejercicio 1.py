169c3d54484380d7497154e81a41b557b49e5ed0
Sergio Andrés Ramírez Céspedes <145515865+Serramirezce@users.noreply.github.com>
2024-02-28 21:46:54 -0500

    Ejercicio1.py

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,12 +1,13 @@
 # Programa de operaciones básicas
 
 print("Utilice \" + \" para suma, \" - \" para resta, \" * \" para producto y \" / \" para cociente")
-num1, num2, operation = input("Ingrese los dos números a operar y la respectiva operación separados por una coma: ").split(",")
+print("Por favor use punto para los decimales y coma únicamente para separar las entradas.")
+num1, num2, operation = input("Ingrese los dos números a operar y la respectiva operación separados por una coma (Ejemplo: \" 2,3,* \"): ").split(",")
 
 x = float(num1)
-y = float(num2)
+y = float(num2)  # Aquí se convierten los dos primeros inputs a números flotantes
 
-def function(x, y, operacion):
+def function(x, y, operacion):    # Esta función realiza la respectiva operación aritmetica que el usuario digite
   if operation == "+":
     return x+y
   elif operation == "-":
@@ -16,4 +17,4 @@ def function(x, y, operacion):
   elif operation == "/":
     return x/y
 if __name__ == "__main__":
-  print(function(x, y, operation))
+  print(function(x, y, operation)) # El print de la función evaluada en las entradas
