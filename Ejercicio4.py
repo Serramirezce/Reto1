8d83e60893a18127fcc72f892b34ea14e3e6fc20
Sergio Andrés Ramírez Céspedes <145515865+Serramirezce@users.noreply.github.com>
2024-02-28 21:55:37 -0500

    Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,15 +1,13 @@
 def largest_sum(nums):
-    max_sum = float('-inf')  # Initialize max_sum with negative infinity
+    max_sum = float('-inf')
 
     for i in range(len(nums) - 1):
-        current_sum = float(nums[i]) + float(nums[i+1])  # Convert strings to floats before adding
-        max_sum = max(max_sum, current_sum)
+        current_sum = float(nums[i]) + float(nums[i+1])  # Convierte entradas de strings a float
+        max_sum = max(max_sum, current_sum)       
 
-    return max_sum
+    return max_sum                                       # Aquí el ciclo retorna la mayor suma de los elementos
 
 if __name__ == "__main__":
   greater = input("Ingrese los números separados por comas: ")
   greater = greater.split(",")
-
-  print(greater)
   print(largest_sum(greater))
